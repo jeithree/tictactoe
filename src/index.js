ff349867bcfa09d85742cb26e88562237591e341
Juan MG <jxithr3@hotmail.com>
2020-08-20 17:05:03 -0500

    changes

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
     // Don't allow user to hit Heroku now that we have a domain
     const host = req.get('Host');
     if (host === 'jeitictactoe.herokuapp.com') {
-        return res.redirect(301, 'https://jeithemes.com/' + req.originalUrl);
+        return res.redirect(301, 'https://jeithemes.com' + req.originalUrl);
     }
     return next();
 });
