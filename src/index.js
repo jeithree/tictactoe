cde9df2205750322edfa556d360701a2057a6929
Juan MG <jxithr3@hotmail.com>
2020-08-20 17:02:02 -0500

    changes

--- src/index.js
+++ src/index.js
@@ -14,6 +14,14 @@ app.enable('trust proxy');
 app.set('port', process.env.PORT || 5000);
 
 // Middlewares
+app.use((req, res, next) => {
+    // Don't allow user to hit Heroku now that we have a domain
+    const host = req.get('Host');
+    if (host === 'jeitictactoe.herokuapp.com') {
+        return res.redirect(301, 'https://jeithemes.com/' + req.originalUrl);
+    }
+    return next();
+});
 app.use(express.json());
 
 // Static files
