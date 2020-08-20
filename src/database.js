0ad8883680a00887c6f600d372399753e9a53a95
Juan MG <jxithr3@hotmail.com>
2020-08-20 14:46:02 -0500

    changes

--- src/database.js
+++ src/database.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
 
 const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
 
 mongoose.connect(DB_CONNECTION_STRING, {
+    user: DB_USER,
+    pass: DB_PASSWORD,
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
