897ba419d64e0e9f434cc877ae5adfde7fc090fe
Juan MG <jxithr3@hotmail.com>
2020-08-20 15:53:20 -0500

    changes

--- src/database.js
+++ src/database.js
@@ -4,7 +4,6 @@ const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 
-
 mongoose.connect(DB_CONNECTION_STRING, {
     user: DB_USER,
     pass: DB_PASSWORD,
